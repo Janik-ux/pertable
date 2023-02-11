02263f642436c33fab040c8f7aca1d5d170cd838
Janik-ux <mianikproductions@gmail.com>
2023-02-11 23:01:25 +0100

    typo fix

--- script.js
+++ script.js
@@ -274,7 +274,7 @@ function share() {
         navigator.clipboard.writeText(link)
             .then(() => {
                 var alert = `<div class="alert alert-primary alert-dismissible fade show" role="alert">
-                                Lick copied to clipboard!
+                                Link copied to clipboard!
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
